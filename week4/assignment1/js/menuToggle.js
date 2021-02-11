a4133639f310ebad7e79922c4d04e3268dcf7473
Saikiran777@ <manchikantisaikiran777@gmail.com>
2021-02-11 22:43:57 +0530

    init

---
+++ week4/assignment1/js/menuToggle.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.menu')
+const navLinks = document.querySelector('.links')
+const menuOpen = document.querySelector("#menu-open")
+const menuClose = document.querySelector("#menu-close")
+
+menu.addEventListener('click', (e) => {
+    menuOpen.classList.toggle('hide')
+    menuClose.classList.toggle('hide')
+})
+
+menuOpen.addEventListener('click',()=>{
+    navLinks.style.right = "0%"
+})
+
+menuClose.addEventListener('click',(e)=>{
+    navLinks.style.right = "100%"
+})
