be63c3fd98e7226023c3071efa881c9bb76317b2
Saikiran777@ <manchikantisaikiran777@gmail.com>
2021-03-08 17:51:34 +0530

    bug fixs and code refactoring

--- week5/assignment2/multiImgCarousel/js/script.js
+++ week5/assignment2/multiImgCarousel/js/script.js
@@ -4,9 +4,8 @@ const carouselmgContainer = document.querySelector('.carousel-images-container')
 
 let direction;
 
-const transitionEnd = (e, transformpercent = 0) => {
+function changeElementsOrder() {
     const logoWidth = parseInt(getComputedStyle(carouselmgContainer.firstElementChild).getPropertyValue('flex-basis'))
-    carouselmgContainer.style.transition = 'none'
     
     if (direction === 1) {
         for (let i = logoWidth; i <= 100; i = i + logoWidth) {
@@ -14,27 +13,35 @@ const transitionEnd = (e, transformpercent = 0) => {
         }
     } else if (direction === -1) {
         for (let i = logoWidth; i <= 100; i = i + logoWidth) {
-            console.log('a')
             carouselmgContainer.prepend(carouselmgContainer.lastElementChild)
         }
     }
-    
-    carouselmgContainer.style.transform = `translateX(${transformpercent}%)`
-    setTimeout(() => carouselmgContainer.style.transition = 'transform 500ms ease-in-out')
 }
 
-prevBtn.addEventListener('click', (e) => {
+prevBtn.addEventListener('click', () => {
+    prevBtn.disabled = true
     direction = -1
-    transitionEnd(undefined, -100)
-    direction = null;
-    setTimeout(() => carouselmgContainer.style.transform = 'translateX(0)')
-
-
+    carouselmgContainer.style.transition = 'none'
+    carouselmgContainer.style.transform = 'translateX(-100%)'
+    changeElementsOrder()
+    setTimeout(() => carouselmgContainer.style.transition = 'transform 500ms ease-in-out')
+    setTimeout(()=>carouselmgContainer.style.transform = 'translateX(0%)') 
 })
 
 nextBtn.addEventListener('click', (e) => {
-    direction = 1;
+    nextBtn.disabled = true
+    direction = 1
     carouselmgContainer.style.transform = 'translateX(-100%)'
 })
 
-carouselmgContainer.addEventListener('transitionend', (e) => transitionEnd(e))
+carouselmgContainer.addEventListener('transitionend', () => {
+    carouselmgContainer.style.transition = 'none'
+    if (direction === 1)
+        changeElementsOrder()
+    carouselmgContainer.style.transform = 'translateX(0%)'
+    setTimeout(() => {
+        carouselmgContainer.style.transition = 'transform 500ms ease-in-out'
+        prevBtn.disabled = false
+        nextBtn.disabled = false
+    })
+})
